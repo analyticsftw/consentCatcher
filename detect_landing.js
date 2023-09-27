ec190b2920c064a66851c9f579d62c03601690a7
Julien Coquet <julien.coquet@gmail.com>
2023-09-27 17:28:15 +0200

    Landing page detector

---
+++ detect_landing.js
@@ -0,0 +1,60 @@
+/**
+ * node.js script that opens a headless browser and scans for cookies dropped without any user interaction
+ * 
+ * Usage:
+ *  node scan.js %URL% %outputfile%
+ *  e.g.
+ *  node scan.js https://www.mightyhive.com mightyhive.json
+*/
+
+
+/* Dependencies */
+fs = require('fs')
+var sf = require('./support_functions.js');
+
+// start URL
+var myArgs = process.argv.slice(2);
+myURL = myArgs[0] ? myArgs[0] : "https://mightyhive.com/";
+filename = myArgs[1] ?  myArgs[1] : "outputs/diageo_allsites.csv";
+//TODO: add support for passing start URL via command line or query string
+
+//define browser type 
+const { chromium } = require('playwright');
+
+const newscan = "";
+console.log("Starting scan for " + myURL);
+startTime = new Date();
+
+// Launching browser, everything below this is async
+(async () => {
+
+  const browser = await chromium.launch({
+    headless: true
+  });
+
+  const context = await browser.newContext({ignoreHTTPSErrors:true});
+  const page = await context.newPage();
+
+  /*
+  // Log and continue all network requests
+  page.route('**', route => {
+    const request = route.request();
+
+    if (request.url() != "undefined") {
+      //rec = logCall(newscan, request.url(), JSON.stringify(request.headers()));
+    }
+    return route.continue();
+  });
+  */
+  
+  await page.goto(myURL);
+  const real_url = page.url();
+  await page.waitForLoadState('networkidle');
+  await browser.close();
+  sf.logHit(filename,myURL+","+real_url);
+  endTime = new Date();
+  scanTime =  endTime - startTime;
+  console.log("Scanned " + myURL + " in " + scanTime+ "s");
+  //TODO update scan time
+
+})();
