507ad98e0f5153a7a2971bbb045bdf4b8863944c
Andres <andres@mightyhive.com>
2023-10-25 17:35:33 +0100

    Add search bar and cookie selection

--- cookie_app/static/src/script.js
+++ cookie_app/static/src/script.js
@@ -5,6 +5,16 @@
 // From: "transform opacity-100 scale-100"
 // To: "transform opacity-0 scale-95"
 
+// function toggleDetailedTable(key) {
+// 	var table = document.getElementById('detailed-' + key);
+// 	if (table.style.display === 'none' || table.style.display === '') {
+// 		table.style.display = 'table-row';
+// 	} else {
+// 		table.style.display = 'none';
+// 	}
+// }
+
+
 document.addEventListener("DOMContentLoaded", function () {
 	const profile = document.getElementById('user-menu-button');
 	const menu = document.querySelector('[aria-labelledby="user-menu-button"]')
@@ -17,20 +27,75 @@ document.addEventListener("DOMContentLoaded", function () {
 		menu.classList.remove('transition', 'ease-out', 'duration-100', 'transform', 'opacity-100', 'scale-100', 'hover:opacity-100', 'hover:scale-100')
 		menu.classList.add('transition', 'ease-out', 'duration-75', 'transform', 'opacity-0', 'scale-95')
 	})
-});
+
+	document.querySelectorAll('tr.cookie_row').forEach(row => {
+		row.addEventListener('click', () => {
+			row.classList.toggle("cookie_selected")
+		});
+	});
 
 
-function toggleDetailedTable(key) {
-	console.log(key)
-	var table = document.getElementById('detailed-' + key);
-	if (table.style.display === 'none' || table.style.display === '') {
-		table.style.display = 'table-row';
-	} else {
-		table.style.display = 'none';
+	allsitesrows = document.querySelectorAll('.siteCookiesRow[data-site]')
+	for(const site of allsitesrows){
+		site.addEventListener('click',(e) => {
+			siteAttr = site.getAttribute('data-site');
+			let table = document.getElementById('detailed-'+siteAttr);
+			if (table.style.display === 'none' || table.style.display === '') {
+				table.style.display = 'table-row';
+			} else {
+				table.style.display = 'none';
+			}
+		});
 	}
-}
 
 
+	// COOKIES SEARCH BAR
+	const searchInput = document.getElementById("default-search");
+	// store name elements in array-like object
+	const namesFromDOM = document.getElementsByClassName("cookie_row");
+	// listen for user events
 
+	let timeout;
+	searchInput.addEventListener("keyup", (event) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			const { value } = event.target;
 
+			// get user search input converted to lowercase
+			const searchQuery = value.toLowerCase();
+
+			for (const nameElement of namesFromDOM) {
+				// store name text and convert to lowercase
+				let name = nameElement.textContent.toLowerCase();
+				// compare current name to search input
+				if (name.includes(searchQuery)) {
+					// found name matching search, display it
+					nameElement.classList.add("visible");
+					nameElement.style.display = "table-row";
+				} else {
+					// no match, don't display name
+					nameElement.classList.remove("visible");
+					nameElement.style.display = "none";
+				}
+			}
+
+			let allSites = document.querySelectorAll('.detailed-table[data-site]');
+			for(const site of allSites){
+				let siteAttr = site.getAttribute('data-site');
+				// console.log(siteAttr);
+				let allVisible = site.querySelectorAll('tbody>tr.visible').length;
+				// console.log(visible_length);
+				// document.querySelector('.siteCookiesRow[data-site="'+ siteAttr + '"] td.column_total_cookies').innerText = allVisible;
+
+				// Hides all the rows without results
+				if(allVisible > 0){
+					document.querySelector('.siteCookiesRow[data-site="'+ siteAttr + '"] td.column_total_cookies').innerText = allVisible;
+					document.querySelector('.siteCookiesRow[data-site="'+ siteAttr + '"]').style.display = "table-row";
+				}else{
+					document.querySelector('.siteCookiesRow[data-site="'+ siteAttr + '"]').style.display = "none";
+				}
+			}
+		}, 300);
+	});
+});
 
